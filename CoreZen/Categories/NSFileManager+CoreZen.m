00abf309947255fdf0c3804be065dddb5aef910f
Zach Nelson <zach@zdnelson.com>
2022-07-18 10:29:42 -0500

    Added categories for NSFileManager, NSNumber, NSURL

---
+++ CoreZen/Categories/NSFileManager+CoreZen.m
@@ -0,0 +1,53 @@
+//
+//  NSFileManager+CoreZen.m
+//  CoreZen
+//
+//  Created by Zach Nelson on 9/27/18.
+//
+
+#import "NSFileManager+CoreZen.h"
+
+@implementation NSFileManager (AlchemistCore)
+
+- (NSURL *)czn_findOrCreateURLForDirectory:(NSSearchPathDirectory)searchPathDirectory
+								  inDomain:(NSSearchPathDomainMask)domain
+					   appendPathComponent:(NSString *)appendComponent
+									create:(BOOL)create
+									 error:(NSError **)outError {
+	// Search for the directory
+	NSURL *url = [NSFileManager.defaultManager URLForDirectory:searchPathDirectory inDomain:domain appropriateForURL:nil create:create error:outError];
+	if (!url) {
+		return nil;
+	}
+	
+	// Append the extra path component
+	if (appendComponent) {
+		url = [url URLByAppendingPathComponent:appendComponent];
+	}
+	
+	// Create the directory if necessary
+	if (create && ![self createDirectoryAtURL:url withIntermediateDirectories:YES attributes:nil error:outError]) {
+		return nil;
+	}
+	
+	if (outError) {
+		*outError = nil;
+	}
+	return url;
+}
+
+- (NSURL *)czn_applicationSupportDirectory {
+	NSString *executableName = NSFileManager.czn_executableName;
+	NSError *error;
+	NSURL *url = [self czn_findOrCreateURLForDirectory:NSApplicationSupportDirectory inDomain:NSUserDomainMask appendPathComponent:executableName create:YES error:&error];
+	if (!url) {
+		NSLog(@"Unable to find or create application support directory: %@", error);
+	}
+	return url;
+}
+
++ (NSString *)czn_executableName {
+	return [NSBundle.mainBundle.infoDictionary objectForKey:(NSString*)kCFBundleExecutableKey];
+}
+
+@end
--- CoreZen/CoreZen.h
+++ CoreZen/CoreZen.h
@@ -15,9 +15,16 @@ FOUNDATION_EXPORT const unsigned char CoreZenVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CoreZen/PublicHeader.h>
 
+#pragma mark - Categories
+#import <CoreZen/NSFileManager+CoreZen.h>
+#import <CoreZen/NSURL+CoreZen.h>
+#import <CoreZen/NSNumber+CoreZen.h>
+
+#pragma mark - Identifiers
 #import <CoreZen/Identifier.h>
 #import <CoreZen/Identifiable.h>
 #import <CoreZen/ObjectIdentifier.h>
+
+#pragma mark - Preferences
 #import <CoreZen/PreferencesWindowController.h>
 #import <CoreZen/PreferenceViewController.h>
-

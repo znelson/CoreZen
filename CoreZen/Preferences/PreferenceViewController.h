99379604e1de224e85da48ee331332bf320d57ac
Zach Nelson <zach@zdnelson.com>
2022-07-18 09:31:30 -0500

    Added prefs adapted from CCNPreferencesWindowController

---
+++ CoreZen/Preferences/PreferenceViewController.h
@@ -0,0 +1,64 @@
+//
+//  PreferenceViewController.h
+//  CoreZen
+//
+//  Created by Zach Nelson on 7/11/22.
+//
+
+// The CZNPreferenceViewController concept was adapted
+// from CCNPreferencesWindowController at:
+// https://github.com/phranck/CCNPreferencesWindowController
+//
+// This file is analogous to CCNPreferencesWindowControllerProtocol.h
+
+//
+//  Created by Frank Gregor on 17.01.15.
+//  Copyright (c) 2015 cocoa:naut. All rights reserved.
+//
+
+/*
+ The MIT License (MIT)
+ Copyright © 2014 Frank Gregor, <phranck@cocoanaut.com>
+ http://cocoanaut.mit-license.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the “Software”), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol CZNPreferenceViewController
+
+// UI display name for preference window
+- (NSString *)preferenceDisplayName;
+
+// Internal unique identifier for preference window
+- (NSString *)preferenceIdentifier;
+
+// Icon to display in the preferences window menu bar
+- (NSImage *)preferenceIcon;
+
+@optional
+
+- (NSString *)preferenceToolTip;
+
+@end
+
+NS_ASSUME_NONNULL_END

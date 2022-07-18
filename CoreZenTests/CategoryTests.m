ae96c15f4fb34aa0b1f1cf7e04b1c1705f5d4c23
Zach Nelson <zach@zdnelson.com>
2022-07-18 16:37:35 -0500

    Migrated a few unit tests
    These build but the dependencies are goofed so tests fail at launch looking for FMDB.framework

---
+++ CoreZenTests/CategoryTests.m
@@ -0,0 +1,62 @@
+//
+//  CategoryTests.m
+//  CoreZenTests
+//
+//  Created by Zach Nelson on 7/18/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@import CoreZen;
+
+@interface CategoryTests : XCTestCase
+
+@end
+
+@implementation CategoryTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testRelativePath {
+	NSURL *urlOne = [NSURL fileURLWithPath:@"/Users/Alchemist" isDirectory:YES];
+	
+	NSString *relativePath = @"Documents/foo.pdf";
+	NSURL *urlTwo = [NSURL URLWithString:relativePath relativeToURL:urlOne];
+	
+	NSString *anotherRelativePath = [urlOne zen_relativePathToURL:urlTwo];
+	if (anotherRelativePath) {
+		XCTAssert([relativePath isEqualToString:anotherRelativePath], @"Relative paths should match, %@ != %@", relativePath, anotherRelativePath);
+	} else {
+		XCTFail(@"ALKGetRelativePath failed, %@ vs %@", urlOne, urlTwo);
+	}
+}
+
+- (void)testVolumeInfo {
+	NSURL *url = [NSURL fileURLWithPath:@"/Users" isDirectory:YES];
+	NSString *volumeName;
+	NSURL *volumeURL;
+	NSUUID *volumeUUID;
+	NSError *error;
+	
+	if ([url zen_volumeName:&volumeName volumeURL:&volumeURL volumeUUID:&volumeUUID error:&error]) {
+		NSString *anotherVolumeName;
+		NSURL *anotherVolumeURL;
+		
+		if ([NSURL zen_volumeInfoForUUID:volumeUUID volumeName:&anotherVolumeName volumeURL:&anotherVolumeURL]) {
+			XCTAssert([volumeName isEqual:anotherVolumeName], @"Volume names should match, %@ != %@", volumeName, anotherVolumeName);
+			XCTAssert([volumeURL isEqual:anotherVolumeURL]);
+		} else {
+			XCTFail(@"ALKFindVolumeInfoForUUID failed, UUID: %@", volumeUUID);
+		}
+	} else {
+		XCTFail(@"ALKGetVolumeInfoForURL failed, URL: %@, error: %@", url, error);
+	}
+}
+
+@end
---
+++ CoreZenTests/ObjectCacheTests.m
@@ -0,0 +1,76 @@
+//
+//  ObjectCacheTests.m
+//  CoreZenTests
+//
+//  Created by Zach Nelson on 7/18/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@import CoreZen;
+#import "TestIdentifiable.h"
+
+@interface AlchemistCoreIdentifiableTests : XCTestCase
+
+@property (strong, nonatomic) ZENObjectCache* cache;
+
+@end
+
+@implementation AlchemistCoreIdentifiableTests
+
+- (void)setUp {
+	// Put setup code here. This method is called before the invocation of each test method in the class.
+	
+	self.cache = [ZENObjectCache weakObjectCache];
+	XCTAssert(self.cache);
+	
+	ZENSetLargestObjectIdentifier(0);
+}
+
+- (void)tearDown {
+	// Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testIdentifiers {
+	ZENIdentifier nextIdentifier = ZENGetNextObjectIdentifier();
+	XCTAssert(nextIdentifier == 1);
+	
+	nextIdentifier = ZENGetNextObjectIdentifier();
+	XCTAssert(nextIdentifier == 2);
+	
+	ZENTestIdentifiable* obj = [ZENTestIdentifiable new];
+	ZENIdentifier objID = obj.identifier;
+	XCTAssert(objID == 3);
+	
+	nextIdentifier = ZENGetNextObjectIdentifier();
+	XCTAssert(nextIdentifier == 4);
+}
+
+- (void)testNonExistingObject {
+	ZENSetLargestObjectIdentifier(2);
+	ZENIdentifier identifier = ZENGetNextObjectIdentifier();
+	XCTAssert(identifier == 3);
+	
+	ZENTestIdentifiable* obj = [ZENTestIdentifiable testIdentifiableWithIdentifier:identifier];
+	[self.cache cacheObject:obj];
+	
+	id<ZENIdentifiable> cachedObject = [self.cache cachedObject:1];
+	XCTAssert(cachedObject == nil);
+}
+
+- (void)testExistingObject {
+	ZENSetLargestObjectIdentifier(201);
+	ZENIdentifier identifier = ZENGetNextObjectIdentifier();
+	XCTAssert(identifier == 202);
+	
+	ZENTestIdentifiable* objOne = [ZENTestIdentifiable testIdentifiableWithIdentifier:identifier];
+	[self.cache cacheObject:objOne];
+	
+	ZENIdentifier objOneID = objOne.identifier;
+	ZENTestIdentifiable* objTwo = [self.cache cachedObject:objOneID];
+
+	XCTAssert(objOne == objTwo);
+	XCTAssert(objOneID == objTwo.identifier);
+}
+
+@end

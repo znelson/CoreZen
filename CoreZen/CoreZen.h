8ab845902043847df779c606aec9923d2ce44eb3
Zach Nelson <zach@zdnelson.com>
2022-07-18 09:16:41 -0500

    Added empty Framework project

---
+++ CoreZen/CoreZen.h
@@ -0,0 +1,18 @@
+//
+//  CoreZen.h
+//  CoreZen
+//
+//  Created by Zach Nelson on 7/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreZen.
+FOUNDATION_EXPORT double CoreZenVersionNumber;
+
+//! Project version string for CoreZen.
+FOUNDATION_EXPORT const unsigned char CoreZenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreZen/PublicHeader.h>
+
+

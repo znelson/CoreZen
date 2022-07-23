66a2828e57d706e4c00cf0ab71a025636aaf352e
Zach Nelson <zach@zdnelson.com>
2022-07-23 10:44:02 -0500

    Added empty MPVController file

---
+++ CoreZen/Player/MPVController.h
@@ -0,0 +1,12 @@
+//
+//  MPVController.h
+//  CoreZen
+//
+//  Created by Zach Nelson on 7/23/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPVController : NSObject
+
+@end
---
+++ CoreZen/Player/MPVController.m
@@ -0,0 +1,14 @@
+//
+//  MPVController.m
+//  CoreZen
+//
+//  Created by Zach Nelson on 7/23/22.
+//
+
+#import "MPVController.h"
+
+#import <mpv/client.h>
+
+@implementation MPVController
+
+@end

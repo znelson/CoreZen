6826ef0275d51ada886f0a36ab3dae185fa1eaf1
Zach Nelson <zach@zdnelson.com>
2022-11-08 16:09:48 -0600

    Empty class for frame renderer

---
+++ CoreZen/Media/FrameRenderer.h
@@ -0,0 +1,12 @@
+//
+//  FrameRenderer.h
+//  CoreZen
+//
+//  Created by Zach Nelson on 11/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZENFrameRenderer : NSObject
+
+@end
---
+++ CoreZen/Media/FrameRenderer.m
@@ -0,0 +1,12 @@
+//
+//  FrameRenderer.m
+//  CoreZen
+//
+//  Created by Zach Nelson on 11/8/22.
+//
+
+#import "FrameRenderer.h"
+
+@implementation ZENFrameRenderer
+
+@end

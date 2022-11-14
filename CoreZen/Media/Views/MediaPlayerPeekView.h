aba639b94127dc63dce01d0fbc8f211a9418f3fa
Zach Nelson <zach@zdnelson.com>
2022-11-14 14:31:37 -0600

    Added peek view

---
+++ CoreZen/Media/Views/MediaPlayerPeekView.h
@@ -0,0 +1,14 @@
+//
+//  MediaPlayerPeekView.h
+//  CoreZen
+//
+//  Created by Zach Nelson on 11/10/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ZENMediaPlayerPeekView : NSView
+
+@property (nonatomic, weak) IBOutlet NSImageView *imageView;
+
+@end

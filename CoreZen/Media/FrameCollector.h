b36e742a74e823e1f13d0937d2ef921bfa6dddc1
Zach Nelson <zach@zdnelson.com>
2022-11-15 11:47:01 -0600

    Moved frame collector to its own file

---
+++ CoreZen/Media/FrameCollector.h
@@ -0,0 +1,18 @@
+//
+//  FrameCollector.h
+//  CoreZen
+//
+//  Created by Zach Nelson on 11/15/22.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreZen/FrameRendererTypes.h>
+
+@interface ZENFrameCollector : NSObject
+
++ (instancetype)frameCollectorWithCount:(NSUInteger)count
+							 completion:(ZENRenderFramesResultsBlock)completion;
+
+- (void)collect:(ZENRenderedFrame *)frame;
+
+@end
